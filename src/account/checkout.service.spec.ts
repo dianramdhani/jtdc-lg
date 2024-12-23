a5e2444ba07bdc7deaa68a6729b2290f9224053f
Dian Ramdhani Lubis <38197188+dianramdhani@users.noreply.github.com>
2024-12-23 11:34:41 +0700

    fix(checkout): adjust method (#4)
    * feat: init login
    
    * feat: init checkout
    
    * feat: implement checkout
    
    * fix: checkout method
    
    * fix(checkout): adjust method

--- src/account/checkout.service.spec.ts
+++ src/account/checkout.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test } from '@nestjs/testing';
 import { CheckoutService } from './checkout.service';
 import { PrismaService } from 'src/database/prisma/prisma.service';
-import { addMinutes, format } from 'date-fns';
 import { Cookie } from 'puppeteer';
 import { ScheduleModule } from '@nestjs/schedule';
 
@@ -28,15 +27,41 @@ describe('CheckoutService', () => {
     'should be checkout',
     async () => {
       const { cookies } = await prismaService.account.findFirst({
-        where: { username: 'aurora.dreamer@gmx.com' },
+        where: { username: 'serenity.petal@gmx.com' },
       });
       // const time = format(addMinutes(new Date(), 1), 'HH:mm');
-      const time = '00:00';
+      const time = '11:30';
       const parsedCookies = JSON.parse(cookies) as Cookie[];
       if (parsedCookies.length) {
         console.log(`Checkout in ${time}`);
         await checkoutService.checkout(parsedCookies, time);
       }
+    },
+    6 * 60 * 60 * 1000,
+  );
+
+  it(
+    'should be checkout all accounts',
+    async () => {
+      const usernames = [
+        'aurora.dreamer@gmx.com',
+        'twilight.spark@gmx.com',
+        'luminous.tracker@gmx.com',
+        'twilight.rover@gmx.com',
+      ];
+      const time = '00:00';
+      const allCookies = await Promise.all(
+        usernames.map(async (username) => {
+          const { cookies } = await prismaService.account.findFirst({
+            where: { username },
+          });
+          return JSON.parse(cookies) as Cookie[];
+        }),
+      );
+
+      await Promise.all(
+        allCookies.map((cookies) => checkoutService.checkout(cookies, time)),
+      );
     },
     6 * 60 * 60 * 1000,
   );
